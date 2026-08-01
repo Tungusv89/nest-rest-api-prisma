7c166c3f9b81e150f46c5d2442528b98a6e67e9d
Evgeny <tungusv89@mail.ru>
2026-08-01 17:57:48 +0600

    add ApiProperty

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,12 +1,15 @@
 // dto/create-task.dto.ts
 import { IsString, MinLength, IsOptional, IsEnum } from 'class-validator';
 import { TaskStatus } from '../entities/task.entity';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateTaskDto {
+  @ApiProperty({ example: 'Купить молоко', minLength: 3 })
   @IsString()
   @MinLength(3)
   title: string;
 
+  @ApiPropertyOptional({ enum: TaskStatus, example: TaskStatus.PENDING })
   @IsOptional()
   @IsEnum(TaskStatus)
   status?: TaskStatus;
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
 // users/dto/create-user.dto.ts
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '123456', minLength: 6 })
   @IsString()
   @MinLength(6)
   password: string;
 
+  @ApiPropertyOptional({ example: 'Иван' })
   @IsOptional()
   @IsString()
   name: string;
